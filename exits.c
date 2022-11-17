705d33611c54b6506b5206a2ccde23808758038c
Abdulrasheed <rashysmooth@yahoo.com>
2022-11-17 02:41:16 +0100

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,57 +1,74 @@
 #include "shell.h"
 
+/**
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
 
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
 
 /**
-
- * _erratoi - converts a string to an integer
-
- * @s: the string to be converted
-
- * Return: 0 if no numbers in string, converted number otherwise
-
- *       -1 on error
-
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
-
-int _erratoi(char *s)
-
+char *_strncat(char *dest, char *src, int n)
 {
+	int i, j;
+	char *s = dest;
 
-int i = 0;
-
-unsigned long int result = 0;
-
-
-
-if (*s == +)
-
-s++;  /* TODO: why does this make main return 255? */
-
-for (i = 0;  s[i] != 0; i++)
-
-{
-
-if (s[i] >= 0 && s[i] <= 9)
-
-{
-
-result *= 10;
-
-result += (s[i] - 0);
-
-if (result > INT_MAX)
-
-return (-1);
-
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
 }
 
-else
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
 
-return (-1);
-
-}
-
-return (result);
-
+	return (NULL);
 }
