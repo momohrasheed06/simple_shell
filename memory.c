ab6acee61bea282ed42bfa26807f67aa3dda7691
Abdulrasheed <rashysmooth@yahoo.com>
2022-11-17 02:46:52 +0100

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,57 +1,18 @@
 #include "shell.h"
 
-
-
 /**
-
- * _erratoi - converts a string to an integer
-
- * @s: the string to be converted
-
- * Return: 0 if no numbers in string, converted number otherwise
-
- *       -1 on error
-
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
  */
-
-int _erratoi(char *s)
-
+int bfree(void **ptr)
 {
-
-int i = 0;
-
-unsigned long int result = 0;
-
-
-
-if (*s == +)
-
-s++;  /* TODO: why does this make main return 255? */
-
-for (i = 0;  s[i] != 0; i++)
-
-{
-
-if (s[i] >= 0 && s[i] <= 9)
-
-{
-
-result *= 10;
-
-result += (s[i] - 0);
-
-if (result > INT_MAX)
-
-return (-1);
-
-}
-
-else
-
-return (-1);
-
-}
-
-return (result);
-
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
 }
